e913d3f6bfd0814ca6b9d7c3f5f1c5cc7b270397
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-11-01 20:47:53 +0800

    _write() some stuff from blink.c
    Proper printf() hangs for whatever reason

--- blink.c
+++ blink.c
@@ -1,14 +1,30 @@
 
 #include "pico/stdlib.h"
+#include "pico/printf.h"
 
 int main() {
 	const uint LED_PIN = 25;
 	gpio_init(LED_PIN);
 	gpio_set_dir(LED_PIN, GPIO_OUT);
+
+	bool status = stdio_init_all();
+
+	if (!status) {
+		while (true) {
+			gpio_put(LED_PIN, 1);
+			sleep_ms(50);
+			gpio_put(LED_PIN, 0);
+			sleep_ms(50);
+		}
+	}
+
 	while (true) {
 		gpio_put(LED_PIN, 0);
+		_write(1, "aaa\n", 4);
 		sleep_ms(250);
 		gpio_put(LED_PIN, 1);
+		_write(1, "bbb\n", 4);
 		sleep_ms(250);
+		//printf("aa\n");
 	}
 }
