cd9f4d7a5765559243b65a543243e3a75458925c
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-11-01 21:02:42 +0800

    We're going to debug this without a debugger

--- blink.c
+++ blink.c
@@ -1,6 +1,21 @@
 
 #include "pico/stdlib.h"
 #include "pico/printf.h"
+#include "pico/bootrom.h"
+
+/*
+src/rp2_common/pico_standard_link/crt0.S:decl_isr_bkpt isr_invalid
+src/rp2_common/pico_standard_link/crt0.S:decl_isr_bkpt isr_nmi
+src/rp2_common/pico_standard_link/crt0.S:decl_isr_bkpt isr_hardfault
+src/rp2_common/pico_standard_link/crt0.S:decl_isr_bkpt isr_svcall
+src/rp2_common/pico_standard_link/crt0.S:decl_isr_bkpt isr_pendsv
+src/rp2_common/pico_standard_link/crt0.S:decl_isr_bkpt isr_systick
+*/
+
+void isr_hardfault(void) {
+	_write(1, "HARDFAULT\n", 10);
+	reset_usb_boot(1 << 25, 0);
+}
 
 int main() {
 	const uint LED_PIN = 25;
@@ -18,13 +33,14 @@ int main() {
 		}
 	}
 
-	while (true) {
+	for (int i = 0; i < 5; i++) {
 		gpio_put(LED_PIN, 0);
 		_write(1, "aaa\n", 4);
 		sleep_ms(250);
 		gpio_put(LED_PIN, 1);
 		_write(1, "bbb\n", 4);
 		sleep_ms(250);
-		//printf("aa\n");
 	}
+
+	printf("aa\n");
 }
