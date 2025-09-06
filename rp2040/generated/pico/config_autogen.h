21debc2f76b4a313c1f730552b03e98dde131669
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2025-09-06 17:52:15 +0800

    Absolute path -> relative path

--- rp2040/generated/pico/config_autogen.h
+++ rp2040/generated/pico/config_autogen.h
@@ -4,7 +4,7 @@
 
 // based on PICO_CONFIG_HEADER_FILES:
 
-#include "/home/pop/Code/rp2_min/pico-sdk/src/boards/include/boards/pico.h"
-#include "/home/pop/Code/rp2_min/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h"
+#include "pico-sdk/src/boards/include/boards/pico.h"
+#include "pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h"
 
 // based on PICO_RP2350_ARM_S_CONFIG_HEADER_FILES:
